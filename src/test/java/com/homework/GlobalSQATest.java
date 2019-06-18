070dc10f0451b50c59d6854c6e76a1e6505210f2
grantconnerywalker <grant.connery.walker@gmail.com>
2019-06-18 16:59:50 -0700

    GlobalSQATest complete

--- src/test/java/com/homework/GlobalSQATest.java
+++ src/test/java/com/homework/GlobalSQATest.java
@@ -4,36 +4,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class GlobalSQATest {
-
-    WebDriver driver;
+public class GlobalSQATest extends SeleniumTest {
 
     @Test
-    public void exampleTest() {
-        System.setProperty("webdriver.chrome.driver", "D:\\Users\\Pills\\seleniumDrivers\\chromedriver.exe");
-        driver = new ChromeDriver();
-//        System.setProperty("webdriver.gecko.driver", "D:\\Users\\Pills\\seleniumDrivers\\geckodriver.exe");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public void globalSQATest() {
+        // Step 0: navigate to page
+        driver.get("http://globalsqa.com/demo-site/draganddrop");
 
-        //open the url or AUT
-        driver.get("http://newtours.demoaut.com/");
+        // Step 1: Drag 2nd image into trash box
+        WebDriverWait waitForFrame = new WebDriverWait(driver, 5);
+        waitForFrame.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector(".demo-frame")));
 
-        //Click on the register link:
-        WebElement lnk_register = driver.findElement(By.linkText("REGISTER"));
-        lnk_register.click();
+        WebElement secondImage = driver.findElement(By.cssSelector("#gallery > li:nth-child(2) > img"));
+        WebElement trash = driver.findElement(By.id("trash"));
 
-        //Enter details on the provided text boxes by using sendkeys method.
-        WebElement txtBox_firstname = driver.findElement(By.name("firstName"));
-        txtBox_firstname.sendKeys("John");
+        Actions action = new Actions(driver);
+        action.dragAndDrop(secondImage, trash).build().perform();
+        // Sleep added to help follow along visually
+        sleep(2);
 
-        //or directly we can identify element and act on that in one line
-        driver.findElement(By.name("lastName")).sendKeys("Clark");
-        driver.findElement(By.name("phone")).sendKeys("2342342341");
-        driver.findElement(By.name("userName")).sendKeys("sunilpatro1985@gmail.com");
+        // Step 2: Recycle same image from trash
+        WebElement recycleButton = driver.findElement(By.xpath("//a[@title='Recycle this image']"));
+        recycleButton.click();
+
+        // Sleep added to help follow along visually
+        sleep(2);
     }
 }
